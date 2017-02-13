aa6635eed20e68f6eb4e0f273e6c6342caa76c2c
omanya <mariaosipenko@yahoo.de>
2017-02-13 14:18:09 +0200

    Update principal_expectile.py

--- principal_expectile.py
+++ principal_expectile.py
@@ -1,13 +1,47 @@
 # -*- coding: utf-8 -*-
 """
-author: Maria Osipenko
+Created on Wed Feb  1 17:39:25 2017
+
+@author: Maria Osipenko
 
 Principal expectile algorithm from:
+    N. Tran, M. Osipenko, W. Haerdle (2014):
+        Principal Component Analysis in an Asymmetric Norm, arXiv:1401.3229
+
+
+Reference list:
+     
+[1] Principal Component Analysis in an Asymmetric Norm, N. Tran, M. Osipenko, W. Haerdle, arXiv:1401.3229
+
+[2]  W.K Newey and J.L. Powell. Asymmetric least squares estimation and testing.
+Econometrica: Journal of the Econometric Society, pages 819–847, 1987.
+
+
+Summary:
+    Principal component analysis (PCA) is a widely used dimension reduction tool
+in the analysis of many kind of high-dimensional data. However, in many of the
+above applications, one is interested in capturing the tail of the data rather than the
+mean. 
+    Pricipal expectile algorithm is an analogue of PCA for expectiles.
+    Expectiles are tail measures analogue quantiles. Both quantiles and expectiles
+can be seen as minimizers of an asymmetric norm:
     
-N. Tran, M. Osipenko, W. Haerdle (2014) "Principal Component Analysis in an Asymmetric Norm", http://arxiv.org/abs/1401.3229
+    m_tau(X,alpha) = argmin E ||X - m ||_tau,alpha
+    
+with ||x||_tau,alpha = sum_j(|x_j|^alpha) ((1-tau)1(x_j<0) + tau 1(x_j>0)). If 
+alpha = 1 we have the definition of tau-quantile, if alpha = 2 we have tau-expectile.
+While a quantile is a generalization of the median, an expectile generalizes the mean.
+    
+    Principal expectile algorithm is a dimension reduction tool for such multivariate 
+expectiles. It is maximizes the tau-variance of the projection, see [1] for 
+further information. 
+    The computation is based on iterative least squares from [2]. 
+
+
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 #%%
 #create a functional library for computing principal expectile
@@ -67,7 +101,7 @@ def psihat(ymat,lab,tau):
 
 #%%      
 def expdir(ymat,psi,tau):
-    """returns the weights of the directional expectile
+    """returns the weights of directional expectile
     """
     
     obs = psi.dot(np.transpose(ymat))
@@ -132,7 +166,7 @@ def compare_basis(basis_fix,basis_old):
     
 #%%
 # example:
-# generate a multivariate sample from a mixture of normals
+# generate a sample from a mixture of distributions
 np.random.seed(1234)#set the seed
 n,p,fvar1,fvar2,mix_prob = 1000,20,8,4,0.8 #dimensions, factor variance, mixing probability
 scr1 = fvar1*np.random.randn(n) #factor scores on f1
@@ -140,15 +174,17 @@ scr2 = fvar2*np.random.randn(n) #factor scores on f2
 f1 = np.sin(np.arange(-np.pi,np.pi,2*np.pi/p)) #factors
 f2 = np.cos(np.arange(-np.pi,np.pi,2*np.pi/p))
 eps = 0.01*np.random.randn(n,p) #error term
+#random draws for mixing
 what_mix = (np.random.uniform(0.,1.,n)>mix_prob)*1
+#construct a factorized sample from the mixture
 yfactor = np.zeros((n,p),np.float)
 for i in range(n):
     yfactor[i] = scr1[i]*f1*what_mix[i]+scr2[i]*f2*(1-what_mix[i])+eps[i]
 
 #%%
-import matplotlib.pyplot as plt
-
-taus = [0.99,0.5] #estimate principal directions for these expectile levels; 0.5 equivalent to PCA
+#estimate the principal directions (prdir) for the expectile levels 0.99,0.5; 0.5 is equivalent to PCA
+taus = [0.99,0.5]
+#true prdir capturing the largest variation (for tau = 0.99 it is f1, for tau = 0.5 it is f2) 
 f,nam = np.vstack((f1,f2)),["f1","f2"]
 #plot parameters
 cols=[["r","g"],["m","b"]]
@@ -157,10 +193,10 @@ lins=[["-","--"],["-","--"]]
 for i in range(len(taus)):
     tau=taus[i]
     np.random.seed(1234+i)
-    psi,_ = prdir(yfactor,tau,iter_tol=10) #estimator for principal direction
+    psi,_ = prdir(yfactor,tau,iter_tol=10) #estimator for prdir
     target_f,col,lin = f[i],cols[i],lins[i]
     ax1 = plt.subplot(111)
-    true = ax1.plot(target_f,color=col[0],ls=lin[0],label = "true"+nam[i]) #real basis
+    true = ax1.plot(target_f,color=col[0],ls=lin[0],label = "true prdir for tau ="+str(tau)) #real basis
     est = ax1.plot(compare_basis(target_f,psi),color=col[1],ls=lin[1],label = "estimate for tau ="+str(tau)) #estimator
     handles, labels = ax1.get_legend_handles_labels()
     ax1.legend(handles, labels)   
